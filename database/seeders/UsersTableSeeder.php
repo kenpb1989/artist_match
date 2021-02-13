fd8d99539793d6f4cefb6777930c2d82dfb4bbc8
kenpg1989 <ken98jazz@yahoo.co.jp>
2021-02-13 16:38:07 +0900

    コミット

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,7 +14,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $param = ['name' => '今井健太'];
-        DB::table('prefs')->insert($param)
+        // $param = ['name' => '今井健太'];
+        // DB::table('prefs')->insert($param)
     }
 }
