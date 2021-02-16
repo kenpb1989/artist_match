dd5bb90c31f5d11dbafaaebfe3a6ce7436191fe4
kenpg1989 <ken98jazz@yahoo.co.jp>
2021-02-16 09:17:56 +0900

    heroku

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -6,8 +6,8 @@
   {{-- <meta name="csrf-token" content="{{ csrf-token }}"> --}}
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-  <script src='{{ asset("js/app.js") }}' defer></script>
+  <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}">
+  <script src='{{ secure_asset("js/app.js") }}' defer></script>
   <title>@yield('title')</title>
 </head>
 <body>
